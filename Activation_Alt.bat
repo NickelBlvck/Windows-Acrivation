b72d5ae5a424ad76d1626b1fcb6f6e9add637e0a
NickelBlvck <61317256+NickelBlvck@users.noreply.github.com>
2025-03-25 18:40:20 +0300

    Add files via upload

---
+++ Activation_Alt.bat
@@ -0,0 +1,13 @@
+@echo off
+:: Проверка на администратора
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Start with Admin...
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~dpnx0' -Verb RunAs"
+    exit /b
+)
+
+:: Основная команда
+echo exec command,please wait...
+powershell -Command "irm https://massgrave.dev/get | iex"
+pause
